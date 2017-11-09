7151defe367c59e79a90b84d3bc8e1f527324731
James Ramsay <james@jramsay.com.au>
2017-11-09 18:57:46 -0500

    fix: missing qstats sha

--- Formula/qstats.rb
+++ Formula/qstats.rb
@@ -4,7 +4,7 @@ class Qstats < Formula
   url "https://github.com/tonyfischetti/qstats/archive/master.tar.gz"
   head "https://github.com/tonyfischetti/qstats.git"
   version "1.0.0"
-  sha256 ""
+  sha256 "cf96288ed3f6b6b4911fe96eee17a63a0eafb09650f5b93315e3b7b0b322d8a2"
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
